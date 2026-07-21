c9815c127a78e585fa19f463062495fa7a775913
mauryask <mauryask1732@gmail.com>
2026-07-21 20:38:38 +0530

    Abtarct factory example updated

--- src/DesignPatterns/CreationalDesign/AbstractFactory/Main.java
+++ src/DesignPatterns/CreationalDesign/AbstractFactory/Main.java
@@ -2,6 +2,7 @@ package DesignPatterns.CreationalDesign.AbstractFactory;
 
 public class Main {
     public static void main(String[] args) {
-
+        Application application = new Application(new MacOSFactory());
+        application.paint();
     }
 }
